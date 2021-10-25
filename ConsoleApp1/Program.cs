a7e3f40f338c4c5b546957050ed9b98b68cbeb8e
back.end.at@dzencode.com <back.end.at.dzencode@google.com>
2021-10-25 16:44:17 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Test();
+            Console.ReadLine();
+        }
+
+        private static void Test()
+        {
+            SomeClass s = null;
+            try
+            {
+                s = new SomeClass();
+            }
+            finally
+            {
+                if (s != null)
+                {
+                    s.Dispose();
+                }
+            }
+        }
+    }
+
+    public class SomeClass : IDisposable
+    {
+        private bool disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("protected virtual void Dispose(bool disposing)");
+                    Console.ReadLine();
+                }
+                disposed = true;
+            }
+        }
+        ~SomeClass()
+        {
+            Dispose(false);
+        }
+    }
+}
